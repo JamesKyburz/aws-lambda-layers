a90681c05dd6fd539a70b9b46241785a08125dc9
James Kyburz <james.kyburz@gmail.com>
2018-12-17 15:24:44 +0100

    build: rename node-custom-lambda -> node-custom-runtime

--- 10.x/build.sh
+++ 10.x/build.sh
@@ -2,5 +2,5 @@
 
 export NODE_VERSION=10.14.2
 
-docker build --build-arg NODE_VERSION -t node-custom-lambda:${NODE_VERSION} .
-docker run --rm node-custom-lambda:${NODE_VERSION} cat /tmp/node-v${NODE_VERSION}.zip > ./layer.zip
+docker build --build-arg NODE_VERSION -t node-custom-runtime:${NODE_VERSION} .
+docker run --rm node-custom-runtime:${NODE_VERSION} cat /tmp/node-v${NODE_VERSION}.zip > ./layer.zip
